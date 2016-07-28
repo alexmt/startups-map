47434ab4ec6ef99f1e2b60b1120a80d82004c64b
Travis CI <travis@testing-worker-linux-docker-49b13cb1-3419-linux-2.prod.travis-ci.org>
2016-07-28 21:13:07 +0000

    Update 2016-07-28T21:13:05.289Z

--- vendor.js
+++ vendor.js
@@ -2276,7 +2276,7 @@ var Reflect;
 })(Reflect || (Reflect = {}));
 //# sourceMappingURL=Reflect.js.map
 /*
- * SystemJS v0.19.31
+ * SystemJS v0.19.35
  */
 (function() {
 function bootstrap() {// from https://gist.github.com/Yaffle/1088850
@@ -2383,7 +2383,7 @@ global.URLPolyfill = URLPolyfill;
   function addToError(err, msg) {
     // parse the stack removing loader code lines for simplification
     if (!err.originalErr) {
-      var stack = (err.stack || err.message || err).toString().split('\n');
+      var stack = ((err.message || err) + (err.stack ? '\n' + err.stack : '')).toString().split('\n');
       var newStack = [];
       for (var i = 0; i < stack.length; i++) {
         if (typeof $__curScript == 'undefined' || stack[i].indexOf($__curScript.src) == -1)
@@ -3762,7 +3762,7 @@ var __exec;
     if (hasBuffer)
       return sourceMapPrefix + new Buffer(sourceMapString).toString('base64');
     else if (typeof btoa != 'undefined')
-      return sourceMapPrefix + btoa(unescape(encodeURIComponent(sourceMap)));
+      return sourceMapPrefix + btoa(unescape(encodeURIComponent(sourceMapString)));
     else
       return '';
   }
@@ -3787,7 +3787,7 @@ var __exec;
         + (load.source.substr(lastLineIndex, 15) != '\n//# sourceURL=' 
           ? '\n//# sourceURL=' + load.address + (sourceMap ? '!transpiled' : '') : '')
         // add sourceMappingURL if load.metadata.sourceMap is set
-        + (sourceMap && inlineSourceMap(sourceMap));
+        + (sourceMap && inlineSourceMap(sourceMap) || '');
   }
 
   var curLoad;
@@ -7370,7 +7370,7 @@ hookConstructor(function(constructor) {
 System = new SystemJSLoader();
 
 __global.SystemJS = System;
-System.version = '0.19.31 Standard';
+System.version = '0.19.35 Standard';
   if (typeof module == 'object' && module.exports && typeof exports == 'object')
     module.exports = System;
 
